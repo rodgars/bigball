80cd65fec676ccbb8286ef46407c5ca2a64921fd
Rodrigo <srodrigogarcia@gmail.com>
2018-06-13 03:58:27 -0300

    fix position profile

--- client/src/actions/RankingActions.js
+++ client/src/actions/RankingActions.js
@@ -5,3 +5,8 @@ export const fetchRanking = () => async dispatch => {
     const res = await axios.get('/api/ranking');
     dispatch({ type: FETCH_RANKING, payload: res.data });
 };
+
+export const fetchRankingById = (id) => async dispatch => {
+    const res = await axios.get(`/api/ranking?id=${id}`);
+    dispatch({ type: FETCH_RANKING, payload: res.data });
+};
--- client/src/components/MyGame/MyGameProfile.js
+++ client/src/components/MyGame/MyGameProfile.js
@@ -1,12 +1,16 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
+import * as actions from '../../actions';
+import _ from 'lodash';
 import {Glyphicon} from 'react-bootstrap';
 import PaymentAlert from '../Utils/PaymentAlert';
 
 const checkRanking = (ranking) => {
     if(ranking == 1) return "Você está na liderança!";
+    else if(ranking == 3) return "Vai ganhar abraço hein!";
     else if(ranking > 1 && ranking <= 5) return "Quase lá, falta pouco!";
-    else return "Não desista, o jogo vira!";
+    else if(ranking > 5 && ranking <= 10) return "Não desista, o jogo vira!";
+    else return "Tá quase igual o random!";
 };
 
 class MyGameProfile extends Component {
@@ -16,8 +20,14 @@ class MyGameProfile extends Component {
         this.state = {};
     }
 
+    componentDidMount(){
+        this.props.fetchRanking();
+    }
+
     render(){
         try{
+            let position = _.find(this.props.ranking, {"_id":this.props.id}).position;
+
             return (
                 <div>
                 <div className="ui card">
@@ -31,7 +41,7 @@ class MyGameProfile extends Component {
                             <span className="date">Criado em {this.props.guess.user.registerDate}</span>
                         </div>
                         <div className="description">
-                            # {this.props.guess.position}º - {checkRanking(this.props.guess.position)} <br />
+                            # {position}º - {checkRanking(position)} <br />
                         </div>
                     </div>
                 </div><br />
@@ -45,8 +55,8 @@ class MyGameProfile extends Component {
     }
 }
 
-function mapStateToProps({guess, auth}){
-    return {guess, auth};
+function mapStateToProps({guess, auth, ranking}){
+    return {guess, auth, ranking};
 }
 
-export default connect(mapStateToProps)(MyGameProfile);
+export default connect(mapStateToProps, actions)(MyGameProfile);
